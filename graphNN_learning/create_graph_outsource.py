b2068d6d4fd83670bb25d0ca489b1c7c1bd6bd2f
可及 <3183346767@qq.com>
2021-09-30 15:44:12 +0800

    gnn

--- graphNN_learning/create_graph_outsource.py
+++ graphNN_learning/create_graph_outsource.py
@@ -1,3 +1,11 @@
 import dgl
 import torch as th
 import scipy.sparse as sp
+spmat=sp.rand(100,100,density=0.05)
+print(spmat)
+print(dgl.from_scipy(spmat))
+
+import networkx as nx
+nx_g=nx.path_graph(5)
+print(nx_g)
+print(dgl.from_networkx(nx_g))
