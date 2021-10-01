228bdfb3251a6a1dd78f34371db79ddb7a0c5be9
可及 <3183346767@qq.com>
2021-10-01 11:50:34 +0800

    123

--- graphNN_learning/ch3/DGLNN.py
+++ graphNN_learning/ch3/DGLNN.py
@@ -38,4 +38,47 @@ class SAGEConv(nn.Module):
             nn.init.xavier_uniform_(self.fc_self.weight,gain=gain)
         nn.init.xavier_uniform_(self.fc_neigh.weight,gain=gain)
 
+    def forward(self,graph,feat):
+        with graph.local_scope():
+            feat_src,feat_dst=expand_as_pair(feat,graph)
+
+    def expand_as_pair(input_,g=None):
+        if isinstance(input_,tuple):
+            return input_
+        elif g is not None and g.is_block:
+            if isinstance(input_,Mapping):
+                input_dst={
+                    k: F.narrow_row(v,0,g.number_of_dst_nodes(k))
+                    for k,v in input_.items()
+                }
+            else:
+                input_dst=F.narrow_row(input_,0,g.number_of_dst_nodes())
+            return input_,input_dst
+        else:
+            return input_,input_
+
+import dgl.function as fn
+import torch.nn.functional as F
+from dgl.utils import check_eq_shape
+
+if self._aggre_type=='mean':
+    graph.srcdata['h']=feat_src
+    graph.update_all(fn.copy_u('h','m'),fn.mean('m','neigh'))
+    h_neigh=graph.dstdata['neigh']
+elif self._aggre_type=='gcn':
+    check_eq_shape(feat)
+    graph.srcdata['h']=feat_src
+    graph.dstdata['h']=feat_dst
+    graph.update_all(fn.copy_u('h','m'),fn.sum('m','neigh'))
+    degs=graph.in_degrees().to(feat_dst)
+    h_neigh=(graph.dstdata['neigh']+graph.dstdata['h'])/(degs.unsqueeze(-1) + 1)
+else:
+    raise KeyError('Aggregator type {} not recognized.'.format(self._aggre_type))
+
+if self._aggre_type=='gcn':
+    rst=self.fc_neigh(h_neigh)
+else:
+    rst=self.fc_self(h_self)+self.fc_neigh(h_neigh)
+
+
 
